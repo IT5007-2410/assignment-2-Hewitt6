25ed7e1ad937db8df6942b143d8a5a8aa53dbe15
SYZ <syz@MacBook-Air.local>
2024-10-10 21:47:13 +0800

    lec progress

--- src/App.jsx
+++ src/App.jsx
@@ -73,6 +73,9 @@ class Delete extends React.Component {
   handleSubmit(e) {
     e.preventDefault();
     /*Q5. Fetch the passenger details from the deletion form and call deleteTraveller()*/
+    const form = document.forms.deleteTraveller;
+    console.log(form.travellername.value);
+    this.props.delfunction(form.travellername.value);
   }
 
   render() {
@@ -125,6 +128,15 @@ class TicketToRide extends React.Component {
 
   deleteTraveller(passenger) {
 	  /*Q5. Write code to delete a passenger from the traveller state variable.*/
+    console.log("deleteTraveller", passenger);
+    var newlist = []
+    this.state.travellers.forEach(element => {
+      if (element.name != passenger) {
+        newlist.push(element)}
+      });
+    console.log(newlist);
+    console.log(this.state.travellers);
+    this.setState({travellers: newlist});
   }
   render() {
     return (
@@ -140,6 +152,7 @@ class TicketToRide extends React.Component {
 		
 		{/*Q4. Code to call the component that adds a traveller.*/}
 		{/*Q5. Code to call the component that deletes a traveller based on a given attribute.*/}
+    <Delete delfunction={this.deleteTraveller} />
 	</div>
       </div>
     );
